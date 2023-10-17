72cb96d5928623219028a3b7bcbbfc0543e25d1a
mechztcc <dojathan@gmail.com>
2023-10-17 16:09:19 -0300

    fix: improviments on env config module

--- src/shared/infra/env-config/env-config.service.spec.ts
+++ src/shared/infra/env-config/env-config.service.spec.ts
@@ -1,18 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { EnvConfigModule } from './env-config.module';
 import { EnvConfigService } from './env-config.service';
 
 describe('EnvConfigService', () => {
-  let service: EnvConfigService;
+  let sut: EnvConfigService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [EnvConfigModule.forRoot()],
       providers: [EnvConfigService],
     }).compile();
 
-    service = module.get<EnvConfigService>(EnvConfigService);
+    sut = module.get<EnvConfigService>(EnvConfigService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should return the variable PORT', () => {
+    expect(sut.getAppPort()).toBe(3000);
+  });
+
+  it('should return the variable NODE_ENV', () => {
+    expect(sut.getNodeEnv()).toBe('test');
   });
 });
