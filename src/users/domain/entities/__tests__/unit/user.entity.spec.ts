74b855d1346c69ce74ab6efb19754678dd4af6ee
mechztcc <dojathan@gmail.com>
2023-10-18 10:43:07 -0300

    feat: user entity tests

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -15,10 +15,26 @@ describe('User entity unit tests', () => {
     sut = new UserEntity(props);
   });
 
-  it('Contructor method', () => {
+  it('Getter of prop name', () => {
+    expect(sut.props.name).toBeDefined();
     expect(sut.props.name).toEqual(props.name);
+    expect(typeof sut.props.name).toBe('string');
+  });
+
+  it('Getter of prop email', () => {
+    expect(sut.props.email).toBeDefined();
     expect(sut.props.email).toEqual(props.email);
+    expect(typeof sut.props.email).toBe('string');
+  });
+
+  it('Getter of prop password', () => {
+    expect(sut.props.password).toBeDefined();
     expect(sut.props.password).toEqual(props.password);
+    expect(typeof sut.props.password).toBe('string');
+  });
+
+  it('Getter of prop createdAt', () => {
+    expect(sut.props.createdAt).toBeDefined();
     expect(sut.props.createdAt).toBeInstanceOf(Date);
   });
 });
--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -9,4 +9,20 @@ export class UserEntity {
   constructor(public readonly props: UserProps) {
     this.props.createdAt = this.props.createdAt ?? new Date();
   }
+
+  get name() {
+    return this.props.name;
+  }
+
+  get password() {
+    return this.props.password;
+  }
+
+  get email() {
+    return this.props.email;
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
+  }
 }
