1af7f074e2fde6f1c90c60b11b515d01226ca63f
mechztcc <dojathan@gmail.com>
2023-10-19 07:58:35 -0300

    feat: added class validator field abstract class

---
+++ src/shared/domain/validators/class-validator-fields.ts
@@ -0,0 +1,28 @@
+import { validateSync } from 'class-validator';
+import {
+  FieldsError,
+  ValidatorsFieldsInterface,
+} from './validator-field.interface';
+
+export abstract class ClassValidatorFields<PropsValidated>
+  implements ValidatorsFieldsInterface<PropsValidated>
+{
+  errors: FieldsError = null;
+  validatedData: PropsValidated = null;
+  validate(data: any): boolean {
+    const errors = validateSync(data);
+
+    if (errors.length) {
+      this.errors = {};
+
+      errors.map((error) => {
+        const field = error.property;
+        this.errors[field] = Object.assign(error.constraints);
+      });
+    } else {
+      this.validatedData = data;
+    }
+
+    return !errors.length;
+  }
+}
